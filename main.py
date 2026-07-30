cf0d1950329cf2f19279078e054dc516db863f1f
Gino Prasad <ginoprasad@gmail.com>
2026-07-29 22:26:25 -0700

    Initialized

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/Users/ginoprasad/miniconda3/bin/python3
+
+import re
+import subprocess as sp
+import settings
+from datetime import datetime
+
+output = sp.run("pmset -g batt", shell=True, capture_output=True).stdout.decode()
+charging_status, battery_percentage = output.strip().split('\n')
+
+charging_status = re.search("(?<=').*?(?=')", charging_status).group()
+battery_percentage = int(re.search("\\S*(?=%)", battery_percentage).group(0))
+
+
+if charging_status != "AC Power" and battery_percentage < settings.battery_percentage_threshold:
+	sp.run(f'"{settings.create_event_path}" "Check Battery On Mac Server!!!" 0', shell=True)
+	print("Battery Low")
+else:
+	print("Battery Fine/Charging")
+
---
+++ settings.py
@@ -0,0 +1,10 @@
+
+
+battery_percentage_threshold = 60
+create_event_path = "/Users/ginoprasad/Scripts/ScheduleManager/create_event.py"
+
+
+
+
+
+
